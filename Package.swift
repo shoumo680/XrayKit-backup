8ce0b2e2328d599bce45cbe4a5378bd6bf38be73
xukj <>
2023-12-01 17:28:12 +0800

    0.1.45 稳定版

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/Cross-Kit/xrayKit/releases/download/0.1.61/XrayKit.xcframework.zip",
-      checksum: "c27a231e665856afa9fc9baf229edc1478c7902ae4fa8e76910deee715d4de0d"
+      url: "https://github.com/ghostwolf90/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
+      checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
     )
   ]
 )
