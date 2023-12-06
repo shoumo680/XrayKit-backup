1d736ef8f7ce2f7905d4c56885fa6dae22ab213d
developersparsh <sparsh.singh23@gmail.com>
2023-12-06 09:43:38 +0530

    Fixed v.0.1.45

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "XrayKit",
-  platforms: [.iOS(.v12), .macOS(.v12)],
-  products: [
-    .library(name: "XrayKit", targets: ["XrayKit"])
-  ],
-  targets: [
-    .binaryTarget(
-      name: "XrayKit",
-      url: "https://github.com/daemeonsama/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
-      checksum: "3c40c25defb67d43a1ec6d1509ed5fb041a8005430648443c68473a32b6aeaa5"
-    )
-  ]
+    name: "XrayKit",
+    platforms: [.iOS(.v12), .macOS(.v12)],
+    products: [
+        .library(name: "XrayKit", targets: ["XrayKit"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "XrayKit",
+            url: "https://github.com/ghostwolf90/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
+            checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
+        )
+    ]
 )
